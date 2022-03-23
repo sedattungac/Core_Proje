67db4dc24f76899c8eb3d653491e3f864f17cad6
Sedat <sedattungac12@gmail.com>
2022-03-23 22:49:30 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/ServiceManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ServiceManager : IServiceService
+    {
+        IServiceDal _serviceDAl;
+        public ServiceManager(IServiceDal serviceDAl)
+        {
+            _serviceDAl = serviceDAl;
+        }
+
+        public void TAdd(Service t)
+        {
+            _serviceDAl.Insert(t);
+        }
+
+        public void TDelete(Service t)
+        {
+            _serviceDAl.Delete(t);
+        }
+
+        public Service TGetById(int id)
+        {
+            return _serviceDAl.GetById(id);
+        }
+
+        public List<Service> TGetList()
+        {
+            return _serviceDAl.GetList();
+        }
+
+        public void TUpdate(Service t)
+        {
+            _serviceDAl.Update(t);
+        }
+    }
+}
---
+++ DataAccessLayer/Migrations/20220323172728_mig2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Link",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Link",
+                table: "Portfolios");
+        }
+    }
+}
