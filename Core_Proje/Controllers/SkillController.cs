cf7b656f4e72476a5e9d91d465489c53a7c57cfa
Sedat <sedattungac12@gmail.com>
2022-03-24 18:10:08 +0300

    viewcomponentler, partialviewler, yetenekler listeleme ve ekleme işlemleri eklendi

---
+++ Core_Proje/Controllers/SkillController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class SkillController : Controller
+    {
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
+        public IActionResult Index()
+        {
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSkill(Skill p)
+        {
+            skillManager.TAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
